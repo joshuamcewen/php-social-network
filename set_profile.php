a61afbbf631c5f24bd9344e80f77c8555b160281
Joshua McEwen <joshuamcewenuk@gmail.com>
2017-11-09 10:55:46 +0000

    Profile sanitisation

--- set_profile.php
+++ set_profile.php
@@ -4,9 +4,9 @@
 // The main job of this script is to execute an INSERT or UPDATE statement to create or update a user's profile information...
 // ... but only once the data the user supplied has been validated on the client-side, and then sanitised ("cleaned") and validated again on the server-side
 // It's your job to add these steps into the code
-// Both sign_up.php and sign_in.php do client-side validation, followed by sanitisation and validation again on the server-side -- you may find it helpful to look at how they work 
+// Both sign_up.php and sign_in.php do client-side validation, followed by sanitisation and validation again on the server-side -- you may find it helpful to look at how they work
 // HTML5 can validate all the profile data for you on the client-side
-// The PHP functions in helper.php will allow you to sanitise the data on the server-side and validate *some* of the fields... 
+// The PHP functions in helper.php will allow you to sanitise the data on the server-side and validate *some* of the fields...
 // ... but you'll also need to add some new PHP functions of your own to validate email addresses and dates
 
 // execute the header script:
@@ -37,69 +37,67 @@ if (!isset($_SESSION['loggedInSkeleton']))
 elseif (isset($_POST['firstname']))
 {
 	// user just tried to update their profile
-	
+
 	// connect directly to our database (notice 4th argument) we need the connection for sanitisation:
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
+
 	// if the connection fails, we need to know, so allow this exit:
 	if (!$connection)
 	{
 		die("Connection failed: " . $mysqli_connect_error);
 	}
-	
+
 	// SANITISATION CODE MISSING:
-	
-	// ...
-	
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
-	$pets = $_POST['pets'];
-	$email = $_POST['email'];
-	$dob = $_POST['dob'];
-	
+
+	$firstname = sanitise($_POST['firstname'], $connection);
+	$lastname = sanitise($_POST['lastname'], $connection);
+	$pets = sanitise($_POST['pets'], $connection);
+	$email = sanitise($_POST['email'], $connection);
+	$dob = sanitise($_POST['dob'], $connection);
+
 	// SERVER-SIDE VALIDATION CODE MISSING:
-	
+
 	// ...
-	
+
 	$errors = "";
-	
+
 	// check that all the validation tests passed before going to the database:
 	if ($errors == "")
-	{		
+	{
 		// read their username from the session:
 		$username = $_SESSION["username"];
-		
+
 		// now write the new data to our database table...
-		
+
 		// check to see if this user already had a favourite:
 		$query = "SELECT * FROM profiles WHERE username='$username'";
-		
+
 		// this query can return data ($result is an identifier):
 		$result = mysqli_query($connection, $query);
-		
+
 		// how many rows came back? (can only be 1 or 0 because username is the primary key in our table):
 		$n = mysqli_num_rows($result);
-			
+
 		// if there was a match then UPDATE their profile data, otherwise INSERT it:
 		if ($n > 0)
 		{
 			// we need an UPDATE:
 			$query = "UPDATE profiles SET firstname='$firstname',lastname='$lastname',pets=$pets,email='$email',dob='$dob' WHERE username='$username'";
-			$result = mysqli_query($connection, $query);		
+			$result = mysqli_query($connection, $query);
 		}
 		else
 		{
 			// we need an INSERT:
 			$query = "INSERT INTO profiles (username,firstname,lastname,pets,email,dob) VALUES ('$username','$firstname','$lastname',$pets,'$email','$dob')";
-			$result = mysqli_query($connection, $query);	
+			$result = mysqli_query($connection, $query);
 		}
 
 		// no data returned, we just test for true(success)/false(failure):
-		if ($result) 
+		if ($result)
 		{
 			// show a successful update message:
 			$message = "Profile successfully updated<br>";
-		} 
+		}
 		else
 		{
 			// show the set profile form:
@@ -115,7 +113,7 @@ elseif (isset($_POST['firstname']))
 		// show an unsuccessful update message:
 		$message = "Update failed, please check the errors above and try again<br>";
 	}
-	
+
 	// we're finished with the database, close the connection:
 	mysqli_close($connection);
 
@@ -123,30 +121,30 @@ elseif (isset($_POST['firstname']))
 else
 {
 	// arrived at the page for the first time, show any data already in the table:
-	
+
 	// read the username from the session:
 	$username = $_SESSION["username"];
-	
+
 	// now read their profile data from the table...
-	
+
 	// connect directly to our database (notice 4th argument):
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
+
 	// if the connection fails, we need to know, so allow this exit:
 	if (!$connection)
 	{
 		die("Connection failed: " . $mysqli_connect_error);
 	}
-	
+
 	// check for a row in our profiles table with a matching username:
 	$query = "SELECT * FROM profiles WHERE username='$username'";
-	
+
 	// this query can return data ($result is an identifier):
 	$result = mysqli_query($connection, $query);
-	
+
 	// how many rows came back? (can only be 1 or 0 because username is the primary key in our table):
 	$n = mysqli_num_rows($result);
-		
+
 	// if there was a match then extract their profile data:
 	if ($n > 0)
 	{
@@ -159,13 +157,13 @@ else
 		$email = $row['email'];
 		$dob = $row['dob'];
 	}
-	
+
 	// show the set profile form:
 	$show_profile_form = true;
-	
+
 	// we're finished with the database, close the connection:
 	mysqli_close($connection);
-	
+
 }
 
 if ($show_profile_form)
@@ -187,7 +185,7 @@ echo <<<_END
   Date of birth: <input type="text" name="dob" value="$dob">
   <br>
   <input type="submit" value="Submit">
-</form>	
+</form>
 _END;
 }
 
