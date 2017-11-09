79b76a0ef40ed3102c9af0fff62c3d417e0e442c
Joshua McEwen <joshuamcewenuk@gmail.com>
2017-11-09 12:41:26 +0000

    Individual profile viewing

--- show_profile.php
+++ show_profile.php
@@ -13,29 +13,33 @@ if (!isset($_SESSION['loggedInSkeleton']))
 }
 else
 {
-	// user is already logged in, read their username from the session:
-	$username = $_SESSION["username"];
-	
-	// now read their profile data from the table...
-	
 	// connect directly to our database (notice 4th argument):
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
+
 	// if the connection fails, we need to know, so allow this exit:
 	if (!$connection)
 	{
 		die("Connection failed: " . $mysqli_connect_error);
 	}
-	
+
+	// Retrieve URL parameter value for username key.
+	if(isset($_GET['username'])) {
+		// Sanitise and set the username.
+		$username = sanitise($_GET['username'], $connection);
+	} else {
+		// If there is not username parameter, assume they're viewing their own profile.
+		$username = $_SESSION["username"];
+	}
+
 	// check for a row in our profiles table with a matching username:
 	$query = "SELECT * FROM profiles WHERE username='$username'";
-	
+
 	// this query can return data ($result is an identifier):
 	$result = mysqli_query($connection, $query);
-	
+
 	// how many rows came back? (can only be 1 or 0 because username is the primary key in our table):
 	$n = mysqli_num_rows($result);
-		
+
 	// if there was a match then extract their profile data:
 	if ($n > 0)
 	{
@@ -50,13 +54,17 @@ else
 	}
 	else
 	{
-		// no match found, prompt user to set up their profile:
-		echo "You still need to set up a profile!<br>";
+		// Prompt user to create a profile if viewing their own and no profile exists...
+		if($username == $_SESSION['username']) {
+			echo "You still need to set up a profile!<br>";
+		} else { // Otherwise, the user their searching for doesn't have a profile.
+			echo "This user's profile doesn't exist.";
+		}
 	}
-	
+
 	// we're finished with the database, close the connection:
 	mysqli_close($connection);
-		
+
 }
 
 // finish off the HTML for this page:
